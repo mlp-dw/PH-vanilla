5a157be306e6f35316d58feccf98fb5f317b6cae
Honzze <95212442+Honzze@users.noreply.github.com>
2022-02-07 11:06:56 +0100

    commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include"./utils/header.php"?>
+    <div class="row mx-auto" style="overflow:hidden">
+        <div class="col-10">
+            <div class="card"style="width: 50rem">
+                <h1>Lorem ipsum dol Quidem, fuga! Totam dolore, provident neque ipsum perspiciatis consequatur autem.</h1>
+            </div>
+        </div>
+
+        <div class="col-2">
+            <div class="card" style="width: 18rem">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, similique libero, nulla, esse magnam dolores inventore laud</h1>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+<?php include"./utils/footer.php" ?>
---
+++ process/process_log.php
@@ -0,0 +1,43 @@
+php <?php
+
+//on teste si tous les champs du formulaire sont remplits
+if (
+    isset($_POST['pseudo']) && !empty($_POST['pseudo'])    
+    )
+    {
+    // ON SE CONNECTE A LA BDD
+        
+    include "../utils/connexion_bdd.php";
+        
+    // ON VERIFIE QUE LE PSEUDO N'EXISTE PAS DÉJà
+    $pseudo = htmlspecialchars($_POST["pseudo"]);  
+
+    $pseudo_verification = $mysqlConnection->prepare("SELECT * FROM users WHERE pseudo = ?");
+    $pseudo_verification->execute([$pseudo]);
+
+    $user = $pseudo_verification->fetch(PDO::FETCH_ASSOC);
+            
+    if($user){
+        header("Location: ../register.php?error=Ce pseudo existe déjà !");
+    }
+    //---------------------------------------------------------------------------------------------
+    // SINON ON LE CREE
+    else{
+
+        $pdoStmnt = $mysqlConnection->prepare("INSERT INTO users (pseudo, created_at) VALUES (?,?)");
+        $isSuccess = $pdoStmnt->execute([$pseudo, date('Y-m-d H:i:s')]);
+                    
+        if (!$isSuccess) {
+            header("Location: ../register.php?error=Echec lors de la connexion à votre compte"); 
+        }else{
+            header("Location: ../index.php?success=Bienvenue sur votre compte !");
+        }
+    
+    }
+        
+}
+else{
+    header("Location: ../register.php?error=Le formulaire n'est pas valide"); 
+}
+
+?>
