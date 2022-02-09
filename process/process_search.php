203ff8b057be3e63b1fdf4f445d174c958acac34
Honzze <95212442+Honzze@users.noreply.github.com>
2022-02-09 16:52:01 +0100

    search

--- process/process_search.php
+++ process/process_search.php
@@ -1,12 +1,34 @@
 <?php
-if(isset($_POST['search']) && !empty($_POST['search']))
-{
-    $_POST["search"] = htmlspecialchars($_POST["search"]); //pour empêcher l'execution de balise
-    $select = $mysqlConnection->prepare("SELECT * FROM products WHERE description LIKE ? OR name LIKE ?");
-    $select->execute([$_POST["search"]."%" , $_POST["search"]."%"]);
+//include "C:\laragon\www\product-hunt\utils\connexion_bdd.php";
+include "D:/laragon/www/PRODUCT-HUNT-main/utils/connexion_bdd.php";
 
-    if (!$select) {
-        header("Location: ../liste-patient.php?error=Echec lors de la recherche"); 
-    }else{
+if(isset($_GET['search']) && !empty($_GET['search']))
+{
+    $_POST['search'] = htmlspecialchars($_GET['search']); //pour empêcher l'execution de balise
+    $select = $mysqlConnection->prepare("SELECT * FROM products WHERE description LIKE ? OR name LIKE ?");
+    $select->execute([$_GET['search'].'%' , $_GET['search'].'%']);
+    $selected = $select->fetchAll();
+    for ($i=0; $i < count($selected); $i++) { 
+        $seeLikes = $mysqlConnection->query("SELECT up
+                                             FROM likes
+                                             WHERE product_id = ' ". $selected[$i]['id'] ."'
+        ");
+        $likes = $seeLikes->fetchAll();
+        $selected[$i]["likes"] = $likes;
+        
     }
-    ?>
+    
+    for ($i=0; $i < count($selected); $i++) { 
+        
+        $seeComments = $mysqlConnection->query("SELECT comment
+                                             FROM likes
+                                             WHERE product_id = ' ". $selected[$i]['id'] ."'
+        ");
+        $comments = $seeComments->fetchAll();
+        $selected[$i]["comments"] = $comments;
+        
+    }
+  
+	echo json_encode($selected);
+};
+?>
---
+++ process/process_see_files_category.php
@@ -0,0 +1,40 @@
+<?php
+// ON CONNECTE A LA BDD
+//include "C:\laragon\www\product-hunt\utils\connexion_bdd.php";
+include "D:/laragon/www/PRODUCT-HUNT-main/utils/connexion_bdd.php";
+
+
+// VOIR LES FICHIERS
+$seeData = $mysqlConnection->query("SELECT *
+										FROM products
+									ORDER BY category
+									");
+$files = $seeData->fetchAll();
+
+for ($i=0; $i < count($files); $i++) { 
+	
+	$seeLikes = $mysqlConnection->query("SELECT up
+										 FROM likes
+										 WHERE product_id = ' ". $files[$i]["id"] ."'
+	");
+	$likes = $seeLikes->fetchAll();
+	$files[$i]["likes"] = $likes;
+	
+}
+
+for ($i=0; $i < count($files); $i++) { 
+	
+	$seeComments = $mysqlConnection->query("SELECT comment
+										 FROM likes
+										 WHERE product_id = ' ". $files[$i]["id"] ."'
+	");
+	$comments = $seeComments->fetchAll();
+	$files[$i]["comments"] = $comments;
+	
+}
+
+
+	echo json_encode($files);
+	
+	
+?>
