6b701a354f5fdf706431b075cd727c0e2eb47d98
mlp-dw <mlp.dwwb@gmail.com>
2022-05-09 11:34:00 +0200

    ---

--- utils/connexion_bdd.php
+++ utils/connexion_bdd.php
@@ -2,7 +2,7 @@
 // connexion a la base de donnée
 try{$mysqlConnection = new PDO(
     'mysql:host=141.94.22.233;dbname=mlpdwwb_product_hunts;charset=utf8', // serveur;base de donnée; encodage de caractère
-    'root', // mon compte à moi pour me connecter au serveur
+    'mlpdwwb', // mon compte à moi pour me connecter au serveur
     'mlpdwwb' // mon mot de passe pour me connecter au serveur
 );
 $mysqlConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
